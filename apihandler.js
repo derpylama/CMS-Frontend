d959a3c1afc3453a278dd4e0f9e2b919a60485bb
derpylama <98833155+derpylama@users.noreply.github.com>
2025-10-14 08:50:17 +0200

    apihandler created

---
+++ apihandler.js
@@ -0,0 +1,10 @@
+class apiHandler{
+    constructor(){
+        this.baseUrl = "https://elias.ntigskovde.se/index.php"
+    }
+
+    getPage(pageKey){
+        fetch(this.baseUrl + "/php/getinfo.php")
+
+    }
+}
