3f521565fd294e5c1f82a270f42e5dcda94b5270
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-10-17 13:59:10 +0200

    add config

---
+++ backend/config.js
@@ -0,0 +1,74 @@
+const { app } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+class ConfigManager {
+    // This class only writes to file if one exists, autoPersist ensures the file exists, i.e ensures persistence
+    constructor(autoPersist = false) {
+        this.file = 'config.json';
+        this.config = {
+            "api": "https://elias.ntigskovde.se/"
+        };
+
+        // If config file exists, load it
+        this.loadConfigFile();
+
+        // If autoPersist is true and config file does not exist, create it
+        if (autoPersist && !this.configFileExists()) {
+            this.writeConfigFile();
+        }
+    }
+
+    getBasePath() {
+        if (app.isPackaged) {
+            return path.dirname(app.getPath('exe'));
+        } else {
+            return path.resolve(__dirname, '..');
+        }
+    }
+
+    getConfigPath() {
+        return path.join(this.getBasePath(), this.file);
+    }
+
+    configFileExists() {
+        return fs.existsSync(this.getConfigPath());
+    }
+
+    readConfigFile() {
+        if (this.configFileExists()) {
+            const data = fs.readFileSync(this.getConfigPath(), 'utf-8');
+            return JSON.parse(data);
+        }
+        return {};
+    }
+
+    writeConfigFile() {
+        if (!this.configFileExists()) {
+            fs.writeFileSync(this.getConfigPath(), JSON.stringify(this.config, null, 4), 'utf-8');
+        }
+    }
+
+    loadConfigFile() {
+        this.config = { ...this.config, ...this.readConfigFile() };
+    }
+
+    get(key) {
+        return this.config[key];
+    }
+
+    set(key, value) {
+        this.config[key] = value;
+        this.writeConfigFile(); // Only writes if file exist
+    }
+
+    getAll() {
+        return this.config;
+    }
+
+    has(key) {
+        return key in this.config;
+    }
+}
+
+module.exports = { ConfigManager };
