38088fd3e89f49611a4f403291c6a7c36c260066
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-10-20 08:22:30 +0200

    Update dimentions

--- backend/main.js
+++ backend/main.js
@@ -9,8 +9,8 @@ const api = new WonkyCMSApiWrapper(config.get("api"));
 
 function createWindow () {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1280,
+        height: 720,
         webPreferences: {
             preload: path.join(__dirname, "preload.js")
         }
